f419d065c3f8fa1d27ed8cc959ae658e8e239fb1
Eduard Bucari <eduard.bucari@gmail.com>
2022-02-21 02:32:09 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/controllers/UserControllerTest.java
+++ src/test/java/ru/job4j/forum/controllers/UserControllerTest.java
@@ -1,13 +1,22 @@
 package ru.job4j.forum.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Application;
+import ru.job4j.forum.models.User;
+import ru.job4j.forum.services.UserService;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -18,24 +27,44 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private UserService users;
 
     @Test
     public void shouldReturnLoginPage() throws Exception {
         mockMvc.perform(
-                get("/login"))
+                        get("/login"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("user/login")
-        );
+                );
     }
 
     @Test
     public void shouldReturnRegisterPage() throws Exception {
         mockMvc.perform(
-                get("/register"))
+                        get("/register"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("user/register")
-        );
+                );
+    }
+
+    @Test
+    public void shouldUserServiceSaveCall() throws Exception {
+        mockMvc.perform(
+                        post("/register")
+                                .param("name","Иванов Иван Иванович")
+                                .param("login","Ivan")
+                                .param("email","ivanov@mail.ru")
+                                .param("password", "password")
+                                .param("checkPassword", "password")
+                ).andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(users, times(1)).saveUser(argument.capture());
+        assertThat(argument.getValue().getName(), is("Иванов Иван Иванович"));
+        assertThat(argument.getValue().getLogin(), is("Ivan"));
+        assertThat(argument.getValue().getEmail(), is("ivanov@mail.ru"));
     }
 }
